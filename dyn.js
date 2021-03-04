ebf56b154ff4aa71ffaf10e57f797fa6996ddb9e
mohammadabuhatab <abu123hatab@gmail.com>
2021-03-04 15:15:49 +0200

    b

--- dyn.js
+++ dyn.js
@@ -1,13 +1,13 @@
 
 var x = prompt("write your first name ");
-var y = prompt("write your secand name");
+var y = prompt("write your second name");
 var c = x+" "+y ;
 var z =prompt("do you like coast citys ?")
 console.log("your full name is :"+x+' '+y);
 document.write("<h1>"+ " welcome here " + c +"</h1>" );
 if(z=="yes")
 {
-    document.write("<h3>" + "you are in right place" + "<h3>")
+    document.write("<h3>" + "you are in the right place" + "<h3>")
 }
 else
 {
