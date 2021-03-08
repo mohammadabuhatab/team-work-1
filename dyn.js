b8bf1dc822ba5a67f72f7162545e658a9ab964ce
mohammadabuhatab <abu123hatab@gmail.com>
2021-03-08 15:15:44 +0200

    a

--- dyn.js
+++ dyn.js
@@ -1,16 +1,42 @@
+var x;
+var y;
+var c;
+var z;
+var a;
 
-var x = prompt("write your first name ");
-var y = prompt("write your second name");
-var c = x+" "+y ;
-var z =prompt("do you like coast citys ?");
+function firstNameWrong() {
+    alert ("we think your name is not true, pleas write your real name");
+     x = prompt("write your first name ");
+ }
+ function secondNameWrong() {
+     alert ("we think your name is not true, pleas write your real name");
+     y = prompt("write your second name");
+ }
+ x = prompt("write your first name ");
+if (x.length<3 )
+{
+    firstNameWrong()
+}
+
+ y = prompt("write your second name");
+
+
+if (y.length<3){
+secondNameWrong()
+}
+
+ c = x+" "+y ;
+ z =prompt("do you like coast citys ?");
 while (z != "yes" && z != "no" )
 {
    z= prompt("do you like coast citys ?(your answer should be yes or no)" );
 }
-var a = prompt ("how many times you visited aqaba?")
+ a = prompt ("how many times you visited aqaba? ")
 
 console.log("your full name is :"+x+' '+y);
+
 document.write("<h1>"+ " welcome here " + c +"</h1>" );
+
 if(z=="yes")
 {
     document.write("<h3>" + "you are in the right place" + "<h3>");
@@ -25,10 +51,6 @@ for (var i =0 ; i<a ; i++)
 {
     document.write ("<img id='smallPhoto' src='images/2.jpg'>");
 }
-function mohammad() {
-   console.log ("i think your information is wronge")
-}
-if (x.length<3 ||y.length<3||a==0)
-{
-    mohammad()
+if (a==0){
+    document.write(" <mark >you shold visit aqaba</mark>") 
 }
